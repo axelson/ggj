a40f9954389b9dcdfb546dc95f127dfb99838451
Jason Axelson <jason.axelson@gmail.com>
2012-01-28 10:56:24 -1000

    Finished the tutorial!

--- breakout/src/Ball.js
+++ breakout/src/Ball.js
@@ -22,6 +22,53 @@ var Ball = cocos.nodes.Node.extend({
         this.scheduleUpdate();
     },
 
+    testBatCollision: function() {
+        var vel = util.copy(this.get('velocity')),
+            ballBox = this.get('boundingBox'),
+
+            // The parent of the ball is the Breakout Layer, which has a 'bat'
+            // property pointing to the player's bat.
+            batBox = this.get('parent').get('bat').get('boundingBox');
+
+        // If moving down then check for collision with the bat
+        if (vel.y > 0) {
+            if (geom.rectOverlapsRect(ballBox, batBox)) {
+                // Flip Y velocity
+                vel.y *= -1;
+            }
+        }
+
+        // Update position and velocity on the ball
+        this.set('velocity', vel);
+    },
+
+    testEdgeCollision: function() {
+        var vel = util.copy(this.get('velocity')),
+            ballBox = this.get('boundingBox'),
+            // Get size of canvas
+            winSize = cocos.Director.get('sharedDirector').get('winSize');
+
+        // Moving left and hit left edge
+        if (vel.x < 0 && geom.rectGetMinX(ballBox) < 0) {
+            // Flip X velocity
+            vel.x *= -1;
+        }
+
+        // Moving right and hit right edge
+        if (vel.x > 0 && geom.rectGetMaxX(ballBox) > winSize.width) {
+            // Flip X velocity
+            vel.x *= -1;
+        }
+
+        // Moving up and hit top edge
+        if (vel.y < 0 && geom.rectGetMinY(ballBox) < 0) {
+            // Flip Y velocity
+            vel.y *= -1;
+        }
+
+        this.set('velocity', vel);
+    },
+
     update: function(dt) {
         var pos = util.copy(this.get('position')),
             vel = util.copy(this.get('velocity'));
@@ -30,6 +77,8 @@ var Ball = cocos.nodes.Node.extend({
         pos.y += dt * vel.y;
 
         this.set('position', pos);
+        this.testBatCollision();
+        this.testEdgeCollision();
     }
 });
 
