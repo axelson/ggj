7f7006ca5f14a28be8b9173b99de0ae5b1909e08
Jason Axelson <jason.axelson@gmail.com>
2012-01-28 13:39:21 -1000

    The snake moves

--- snake-chase/src/Snake.js
+++ snake-chase/src/Snake.js
@@ -1,8 +1,11 @@
 var cocos = require('cocos2d');
 var geom = require('geometry');
+var util = require('util');
 var doublyLinkedList = require('./DoublyLinkedList');
 
 var Snake = cocos.nodes.Node.extend({
+    velocity: null,
+
     init: function() {
         Snake.superclass.init.call(this);
 
@@ -22,6 +25,9 @@ var Snake = cocos.nodes.Node.extend({
         this.set('contentSize', sprite.get('contentSize'));
         this.set('contentSize', spriteHead.get('contentSize'));
 
+        this.set('velocity', new geom.Point(60, 120));
+        this.scheduleUpdate();
+
         var test = doublyLinkedList;
         var test2 = new test.DoublyLinkedList();
         test2.add("Hi");
@@ -29,6 +35,16 @@ var Snake = cocos.nodes.Node.extend({
         console.log(test2);
         console.log(test2.toString());
         console.log("added new stuff2");
+    },
+
+    update: function(dt) {
+        var pos = util.copy(this.get('position')),
+            vel = util.copy(this.get('velocity'));
+
+        pos.x += dt * vel.x;
+        pos.y += dt * vel.y;
+
+        this.set('position', pos);
     }
 });
 
