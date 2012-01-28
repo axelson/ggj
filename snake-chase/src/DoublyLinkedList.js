ff8daeea6b0be824c4c11a15c7df5ebafe5f3ba7
Jason Axelson <jason.axelson@gmail.com>
2012-01-28 13:33:16 -1000

    Remove end of line whitespace

--- snake-chase/src/DoublyLinkedList.js
+++ snake-chase/src/DoublyLinkedList.js
@@ -1,17 +1,17 @@
 /*
  * Doubly Linked List implementation in JavaScript
  * Copyright (c) 2009 Nicholas C. Zakas
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -20,7 +20,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
- 
+
 /**
  * A linked list implementation in JavaScript.
  * @class DoublyLinkedList
@@ -35,29 +35,29 @@ function DoublyLinkedList() {
      * @private
      */
     this._head = null;
-    
+
     /**
      * Pointer to last item in the list.
      * @property _tail
      * @type Object
      * @private
-     */    
+     */
     this._tail = null;
-    
+
     /**
      * The number of items in the list.
      * @property _length
      * @type int
      * @private
-     */    
+     */
     this._length = 0;
 }
 
 DoublyLinkedList.prototype = {
 
     //restore constructor
-    constructor: DoublyLinkedList,    
-    
+    constructor: DoublyLinkedList,
+
     /**
      * Appends some data to the end of the list. This method traverses
      * the existing list and places the value at the end in a new item.
@@ -66,56 +66,56 @@ DoublyLinkedList.prototype = {
      * @method add
      */
     add: function (data){
-    
+
         //create a new item object, place data in
-        var node = { 
-                data: data, 
+        var node = {
+                data: data,
                 next: null,
                 prev: null
             };
-    
+
         //special case: no items in the list yet
         if (this._length == 0) {
             this._head = node;
             this._tail = node;
         } else {
-        
+
             //attach to the tail node
             this._tail.next = node;
             node.prev = this._tail;
             this._tail = node;
-        }        
-        
+        }
+
         //don't forget to update the count
         this._length++;
-    
+
     },
 
     /**
      * Retrieves the data in the given position in the list.
-     * @param {int} index The zero-based index of the item whose value 
+     * @param {int} index The zero-based index of the item whose value
      *      should be returned.
      * @return {variant} The value in the "data" portion of the given item
      *      or null if the item doesn't exist.
      * @method item
      */
     item: function(index){
-    
+
         //check for out-of-bounds values
         if (index > -1 && index < this._length){
             var current = this._head,
                 i = 0;
-                
+
             while(i++ < index){
-                current = current.next;            
+                current = current.next;
             }
-        
+
             return current.data;
         } else {
             return null;
         }
     },
-    
+
     /**
      * Removes the item from the given location in the list.
      * @param {int} index The zero-based index of the item to remove.
@@ -124,17 +124,17 @@ DoublyLinkedList.prototype = {
      * @method remove
      */
     remove: function(index){
-    
+
         //check for out-of-bounds values
         if (index > -1 && index < this._length){
-        
+
             var current = this._head,
                 i = 0;
-                
+
             //special case: removing first item
             if (index === 0){
                 this._head = current.next;
-                
+
                 /*
                  * If there's only one item in the list and you remove it,
                  * then this._head will be null. In that case, you should
@@ -147,36 +147,36 @@ DoublyLinkedList.prototype = {
                 } else {
                     this._head.prev = null;
                 }
-       
+
             //special case: removing last item
             } else if (index === this._length -1){
                 current = this._tail;
                 this._tail = current.prev;
                 this._tail.next = null;
             } else {
-        
+
                 //find the right location
                 while(i++ < index){
-                    current = current.next;            
+                    current = current.next;
                 }
-            
+
                 //skip over the item to remove
                 current.prev.next = current.next;
             }
-        
+
             //decrement the length
             this._length--;
-        
+
             //return the value
-            return current.data;            
-        
+            return current.data;
+
         } else {
             return null;
         }
-               
-    
-    },    
-    
+
+
+    },
+
    /**
      * Returns the number of items in the list.
      * @return {int} The number of items in the list.
@@ -185,7 +185,7 @@ DoublyLinkedList.prototype = {
     size: function(){
         return this._length;
     },
-    
+
     /**
      * Converts the list into an array.
      * @return {Array} An array containing all of the data in the list.
@@ -194,15 +194,15 @@ DoublyLinkedList.prototype = {
     toArray: function(){
         var result = [],
             current = this._head;
-        
+
         while(current){
             result.push(current.data);
             current = current.next;
         }
-        
+
         return result;
     },
-    
+
     /**
      * Converts the list into a string representation.
      * @return {String} A string representation of the list.
@@ -210,7 +210,7 @@ DoublyLinkedList.prototype = {
      */
     toString: function(){
         return this.toArray().toString();
-    }    
+    }
 };
 
 
