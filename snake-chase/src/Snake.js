a279168e4f94ab6e727b5543a678a1a7cee35678
Jason Axelson <jason.axelson@gmail.com>
2012-01-28 12:18:10 -1000

    Add initial snake head

--- snake-chase/src/Snake.js
+++ snake-chase/src/Snake.js
@@ -5,14 +5,21 @@ var Snake = cocos.nodes.Node.extend({
     init: function() {
         Snake.superclass.init.call(this);
 
+        var spriteHead = cocos.nodes.Sprite.create({
+            file: '/resources/sprites.png',
+            rect: new geom.Rect(96, 0, 16, 16)
+        });
         var sprite = cocos.nodes.Sprite.create({
             file: '/resources/sprites.png',
             rect: new geom.Rect(64, 0, 16, 16)
         });
 
         sprite.set('anchorPoint', new geom.Point(0, 0));
+        spriteHead.set('anchorPoint', new geom.Point(-1, 0));
         this.addChild({child: sprite});
+        this.addChild({child: spriteHead});
         this.set('contentSize', sprite.get('contentSize'));
+        this.set('contentSize', spriteHead.get('contentSize'));
     }
 });
 
